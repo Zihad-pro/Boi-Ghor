63b67cc41e790f2655e03868ad7a568f30f7ba15
Zihad-pro <zubaedhasan46@gmail.com>
2025-08-28 00:13:44 +0600

    first commit

---
+++ Boi-Ghor-Client/src/router/router.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
+export default router;
